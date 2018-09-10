6fd499adb8aa4876a40195c6d2e9500bbd264db1
Robert L Quigley <lexm@pobox.com>
2018-09-10 19:42:22 -0400

    Add functions

--- src/models/createdb.js
+++ src/models/createdb.js
@@ -35,11 +35,98 @@ const tables = [
 	}
 ]
 
+const functions = [
+	{
+		name: 'p_user',
+		query: `
+			CREATE OR REPLACE FUNCTION p_user
+				(username_in VARCHAR(200),
+				 lastname_in VARCHAR(200),
+				 firstname_in VARCHAR(200))
+			RETURNS Boolean
+			AS
+			$$
+			BEGIN
+				IF EXISTS (
+					SELECT 1
+					FROM users u
+					WHERE u.username = username_in
+				)
+				THEN
+					RETURN FALSE;
+				END IF;
+
+				INSERT INTO users
+				(
+					username,
+					lastname,
+					firstname,
+					created_on
+				)
+				SELECT username_in,
+							 lastname_in,
+							 firstname_in,
+							 CURRENT_TIMESTAMP;
+
+				RETURN TRUE;
+			END
+			$$ LANGUAGE 'plpgsql';
+		`
+	},
+	{
+		name: 'p_userPassword',
+		query: `
+			CREATE OR REPLACE FUNCTION  p_userPassword
+				 (userid INTEGER,
+					hash VARCHAR(60))
+			RETURNS Boolean
+			AS
+			$$
+			BEGIN
+				IF NOT EXISTS (
+					SELECT 1
+					FROM users u
+					WHERE u.id = userid
+				)
+				THEN
+					RETURN FALSE;
+				END IF;
+
+				INSERT INTO authentications
+				(
+					user_id,
+					password,
+					last_changed,
+					change_on_login
+				)
+				SELECT	userid,
+								hash,
+								CURRENT_TIMESTAMP,
+								FALSE
+				ON CONFLICT (user_id) DO
+				UPDATE
+					SET hash = EXCLUDED.hash,
+							last_change = CURRENT_TIMESTAMP,
+							change_on_login = FALSE;
+				RETURN TRUE;
+			END
+			$$ LANGUAGE 'plpgsql';
+		`
+	}
+]
+
 async function create_tables() {
 	tables.forEach(async (t) => {
 		await pool.query(t.query)
 	})
 }
 
+async function create_functions() {
+	functions.forEach(async (f) => {
+		await pool.query(f.query);
+	})
+}
+
 create_tables();
+create_functions();
 pool.end();
